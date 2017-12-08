1d6bdd1c3c58ffeaf2fb00323882675c3d1b802f
Administrator <zhaoyangtang@jieyuechina.com>
2017-12-08 15:31:25 +0800

    两种方式的沉浸式状态栏

--- app/src/main/java/com/pdscjxy/serverapp/activity/base/BaseActivity.java
+++ app/src/main/java/com/pdscjxy/serverapp/activity/base/BaseActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.AnimRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
@@ -18,12 +19,18 @@ import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.pdscjxy.serverapp.util.OsUtil;
 import com.pdscjxy.serverapp.util.StatusBarUtil;
 
 import com.pdscjxy.serverapp.R;
 import com.pdscjxy.serverapp.permission.EasyPermissions;
 import com.pdscjxy.serverapp.util.Logger;
 import com.pdscjxy.serverapp.view.ProgressDialog;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
 import butterknife.ButterKnife;
 
@@ -84,11 +91,14 @@ public class BaseActivity extends AppCompatActivity implements IActivity, EasyPe
     private String showMessage = "";
     private int dialogId = -1;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-    }
+    private View mViewStatusBarPlace;
 
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(R.anim.activity_right_in, R.anim.activity_right_out);
+    }
 
     @Override
     protected void onDestroy() {
@@ -101,15 +111,18 @@ public class BaseActivity extends AppCompatActivity implements IActivity, EasyPe
     public void setContentView(int layoutResID) {
         // TODO Auto-generated method stub
         super.setContentView(R.layout.activity_base);
+        mViewStatusBarPlace = findViewById(R.id.view_status_bar_place);
         if (layoutResID < 0) {
             return;
         }
         setView(layoutResID);
         ButterKnife.bind(this);
         initTitleBar();
+        // TODO: 2017/12/8 条装
+        setImmersiveStatusBar(true, getResources().getColor(R.color.color_EAEAEA));
+        // TODO: 2017/12/8 图片类型 
+//        StatusBarUtil.setTranslucentForImageView(BaseActivity.this, 0);
 
-        
-        StatusBarUtil.setTranslucentForImageView(BaseActivity.this, 0);
 //        StatusBarUtil.setColor(this, getResources().getColor(R.color.colorPrimary));
 
 //        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -133,6 +146,106 @@ public class BaseActivity extends AppCompatActivity implements IActivity, EasyPe
 //    void onBack() {
 //        finish();
 //    }
+
+    /**
+     * 设置沉浸式状态栏
+     *
+     * @param fontIconDark 状态栏字体和图标颜色是否为深色
+     */
+    protected void setImmersiveStatusBar(boolean fontIconDark, int statusBarPlaceColor) {
+        setTranslucentStatus();
+        if (fontIconDark) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                    || OsUtil.isMIUI()
+                    || OsUtil.isFlyme()) {
+                setStatusBarFontIconDark(true);
+            } else {
+                if (statusBarPlaceColor == Color.WHITE) {
+                    statusBarPlaceColor = 0xffcccccc;
+                }
+            }
+        }
+        setStatusBarPlaceColor(statusBarPlaceColor);
+    }
+
+
+    private void setStatusBarPlaceColor(int statusColor) {
+        if (mViewStatusBarPlace != null) {
+            mViewStatusBarPlace.setBackgroundColor(statusColor);
+        }
+    }
+
+    /**
+     * 设置状态栏透明
+     */
+    private void setTranslucentStatus() {
+
+        // 5.0以上系统状态栏透明
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+    }
+    /**
+     * 设置Android状态栏的字体颜色，状态栏为亮色的时候字体和图标是黑色，状态栏为暗色的时候字体和图标为白色
+     *
+     * @param dark 状态栏字体是否为深色
+     */
+    private void setStatusBarFontIconDark(boolean dark) {
+        // 小米MIUI
+        try {
+            Window window = getWindow();
+            Class clazz = getWindow().getClass();
+            Class layoutParams = Class.forName("android.view.MiuiWindowManager$LayoutParams");
+            Field field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_DARK_MODE");
+            int darkModeFlag = field.getInt(layoutParams);
+            Method extraFlagField = clazz.getMethod("setExtraFlags", int.class, int.class);
+            if (dark) {    //状态栏亮色且黑色字体
+                extraFlagField.invoke(window, darkModeFlag, darkModeFlag);
+            } else {       //清除黑色字体
+                extraFlagField.invoke(window, 0, darkModeFlag);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 魅族FlymeUI
+        try {
+            Window window = getWindow();
+            WindowManager.LayoutParams lp = window.getAttributes();
+            Field darkFlag = WindowManager.LayoutParams.class.getDeclaredField("MEIZU_FLAG_DARK_STATUS_BAR_ICON");
+            Field meizuFlags = WindowManager.LayoutParams.class.getDeclaredField("meizuFlags");
+            darkFlag.setAccessible(true);
+            meizuFlags.setAccessible(true);
+            int bit = darkFlag.getInt(null);
+            int value = meizuFlags.getInt(lp);
+            if (dark) {
+                value |= bit;
+            } else {
+                value &= ~bit;
+            }
+            meizuFlags.setInt(lp, value);
+            window.setAttributes(lp);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // android6.0+系统
+        // 这个设置和在xml的style文件中用这个<item name="android:windowLightStatusBar">true</item>属性是一样的
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (dark) {
+                getWindow().getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            }
+        }
+    }
+
 
     /**
      * 设置页面最外层布局 FitsSystemWindows 属性
